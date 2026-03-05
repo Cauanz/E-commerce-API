ef8e4c36fa8f99285ce6753d2c379f000823a375
Cauanz <61758981+Cauanz@users.noreply.github.com>
2026-03-05 19:05:56 -0300

    ✨ Atualiza lógica de conexão com o banco de dados e aprimora script de teste no package.json

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,14 @@ require("dotenv").config();
 const router = require("./routes/router");
 const sequelize = require("./config/sequelize.db");
 
+const User = require("./models/User");
+const Cart = require("./models/Cart");
+const CartItem = require("./models/CartItem");
+const Order = require("./models/Order");
+const OrderItem = require("./models/OrderItem");
+const Product = require("./models/Product");
+const Payment = require("./models/Payment");
+
 const app = express();
 
 app.use(cors());
@@ -14,10 +22,12 @@ app.use("/v1", router);
 app.listen(process.env.PORT, async () => {
   try {
     await sequelize.authenticate();
+    await sequelize.sync();
     console.log("Connection has been established successfully.");
-    sequelize.sync();
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
   console.log(`Listening on port: ${process.env.PORT}`);
 });
+
+module.exports = app;
